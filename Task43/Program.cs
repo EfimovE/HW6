d0f0ec95f64fb2eb498265d11a101c2703bd4ffe
EfimovE <efimov_e@mail.ru>
2022-08-25 23:51:10 +0500

    save1

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -3,6 +3,16 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+void CommonPointLines (double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2) return "Прямые параллельные.";
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    // Console.WriteLine(x);
+    // Console.WriteLine(y);
+    Console.WriteLine($"Точка пересечения двух прямых ({x}; {y}).");
+}
+
 Console.WriteLine("Введите значение b1 и нажмите Enter.");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение k1 и нажмите Enter.");
@@ -11,9 +21,4 @@ Console.WriteLine("Введите значение b2 и нажмите Enter.")
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение k2 и нажмите Enter.");
 double k2 = Convert.ToDouble(Console.ReadLine());
-
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
-Console.WriteLine(x);
-Console.WriteLine(y);
-Console.WriteLine($"Точка пересечения двух прямых ({x}; {y}).");
+CommonPointLines (b1, k1, b2, k2);
